83c075ea21e6a764de2c130670c57920de0cb7df
Mohamed Ibrahim <mohamed.ibrahim@freshworks.com>
2021-04-19 09:59:52 +0530

    Refactored Subscription to Billing::Subscription

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -4,7 +4,7 @@ class Tenant < ApplicationRecord
   include TenantCacheConcern
 
   has_one :domain_config, dependent: :delete
-  has_one :subscription, dependent: :delete
+  has_one :subscription, class_name: 'Billing::Subscription', dependent: :delete
 
   has_many :tags, dependent: :delete_all
   has_many :tickets, dependent: :delete_all
